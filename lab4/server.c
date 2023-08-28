e7667e5de0d69f9396f84b94134a5825e98f997d
Aditya14-g <128630970+Aditya14-g@users.noreply.github.com>
2023-08-29 01:46:48 +0530

    Add files via upload

---
+++ lab4/server.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+int main()
+{
+
+    // SOCKET
+    int cid = socket(AF_INET, SOCK_DGRAM, 0);
+    if (cid == -1)
+    {
+        printf(" Server is not Ready  ");
+        return 0;
+    }
+
+    // BIND
+    struct sockaddr_in sock;
+	sock.sin_family = AF_INET;
+	sock.sin_port = 1030;
+	sock.sin_addr.s_addr = inet_addr("10.0.2.15");
+	
+	int bindInt = bind(cid, (struct sockaddr *)&sock, sizeof(sock));
+	    if (bindInt == -1)
+	    {
+		printf(" -------------Bind Uncessfull--------------\n ");
+		return 0;
+	    }
+	    
+	  struct sockaddr_in from;
+	  char data[100];
+	  int recv,k;
+	 recv = recvfrom(cid , data , 100, 0 , (struct sockaddr*)&from , &k);
+	 if(recv==-1)
+	 {
+	 	printf(" Error on recv ");
+	 	return 0;
+	 }
+	 printf(" %s",data);
+	 
+
+    // close(cid);
+    printf(" Khudos SERVER  is ready ");
+    return 0;
+}
